7ea7c6afbc6915303256715fe76fdc0fac8c81be
Victor Bruneli <vic.bruneli@hotmail.com>
2021-02-15 11:32:13 -0300

    Treino java

---
+++ Java/aula-java/src/treinee/MenuBasic.java
@@ -0,0 +1,65 @@
+
+package treinee;
+
+import java.util.Scanner;
+
+
+public class MenuBasic {
+        public static void menu(){
+        System.out.println("\tCadastro de clientes");
+        System.out.println("0. Fim");
+        System.out.println("1. Inclui");
+        System.out.println("2. Altera");
+        System.out.println("3. Exclui");
+        System.out.println("4. Consulta");
+        System.out.println("Opcao:");
+    }
+
+    public static void inclui(){
+        System.out.println("Você entrou no método Inclui.");
+    }
+    
+    public static void altera(){
+        System.out.println("Você entrou no método Altera.");
+    }
+    
+    public static void exclui(){
+        System.out.println("Você entrou no método Exclui.");
+    }
+    
+    public static void consulta(){
+        System.out.println("Você entrou no método Consulta.");
+    }
+    
+    public static void main(String[] args) {
+        int opcao;
+        Scanner entrada = new Scanner(System.in);
+        
+        do{
+            menu();
+            opcao = entrada.nextInt();
+            
+            switch(opcao){
+            case 1:
+                inclui();
+                break;
+                
+            case 2:
+                altera();
+                break;
+                
+            case 3:
+                exclui();
+                break;
+                
+            case 4:
+                consulta();
+                break;
+            
+            default:
+                System.out.println("Opção SAIR.");
+            }
+        } while(opcao != 0);
+    }
+}
+
--- Java/aula-java/src/treinee/TreinoEx.java
+++ Java/aula-java/src/treinee/TreinoEx.java
@@ -4,22 +4,31 @@ package treinee;
 //import java.text.DecimalFormat;
 import java.util.Scanner;
 
+/*5. Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
+calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
+(1SM=R$788,00)*/
+
 public class TreinoEx {
         public static void main(String[] args) {
-            
-            Scanner sc = new Scanner(System.in);
-             
-/*4. Escrever um algoritmo que lê:
-- a porcentagem do IPI a ser acrescido no valor das peças
-- o código da peça 1, valor unitário da peça 1, quantidade de peças 1
-- o código da peça 2, valor unitário da peça 2, quantidade de peças 2
-O algoritmo deve calcular o valor total a ser pago e apresentar o resultado.
-Fórmula : (valor1*quant1 + valor2*quant2)*(IPI/100 + 1)*/
+   
+               
            
-
-            sc.close();
-        }
+           Scanner sc = new Scanner(System.in);
+         
+           
+           int minimo=788;
+           System.out.println("Digite o valor do salário de um usuário");
+           int salarioUsuario = sc.nextInt();
+           
+           int calc = (salarioUsuario/minimo); 
+           System.out.println("Quantidade de salario minimos =" +calc);
+                   
+                   
+            sc .close();
+       }
 }
+
+
 /* COisas Basicas
 
 1. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e
@@ -76,6 +85,68 @@ números 4, 5 e 6. A soma das duas médias. A média das médias.
 - o código da peça 2, valor unitário da peça 2, quantidade de peças 2
 O algoritmo deve calcular o valor total a ser pago e apresentar o resultado.
 Fórmula : (valor1*quant1 + valor2*quant2)*(IPI/100 + 1)
+
+ public static int ITI = 10;
+        
+        public static void menu(){
+        System.out.println("\t MENU PECAS");
+        System.out.println("0. Fim");
+        System.out.println("1. peca 01");
+        System.out.println("2. peca 02");
+        System.out.println("3. resultado");
+        System.out.println("Opcao:");
+    }
+
+    public static void pecaUm(){
+        System.out.println("Insira o valor da peca 01.");
+        int a;
+        System.out.println("Quantidade A");
+        int quantA;
+    }
+    
+    public static void pecaDois(){
+        System.out.println("Insira o valor da peca 02.");
+        int b;
+        System.out.println("Quantidade B");
+        int quantB;
+    }
+    
+    public static void resultado(){
+        System.out.println("RESULTADO.");
+        int a; 
+        int b;
+        int quantA;
+        int quantB;
+        int soma = (a*quantA + b*quantB)*(IPI/100 + 1);
+    }
+       
+    public static void main(String[] args) {
+        int opcao;
+        Scanner entrada = new Scanner(System.in);
+        
+        do{
+            menu();
+            opcao = entrada.nextInt();
+            
+            switch(opcao){
+            case 1:
+                pecaUm();
+                break;
+                
+            case 2:
+                pecaDois();
+                break;
+                
+            case 3:
+                resultado();
+                break;
+                
+          
+            default:
+                System.out.println("Opção SAIR.");
+            }
+        } while(opcao != 0);
+    }
 
 5. Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
 calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
