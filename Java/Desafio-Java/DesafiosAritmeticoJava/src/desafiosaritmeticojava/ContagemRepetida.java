3c3bc5c2b72154eee633d33fa73c7204b11fb8d6
Victor Bruneli <vic.bruneli@hotmail.com>
2021-04-12 10:37:29 -0300

    Ex resolvido

--- Java/Desafio-Java/DesafiosAritmeticoJava/src/desafiosaritmeticojava/ContagemRepetida.java
+++ Java/Desafio-Java/DesafiosAritmeticoJava/src/desafiosaritmeticojava/ContagemRepetida.java
@@ -1,7 +1,25 @@
-
-package desafiosaritmeticojava;
-
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.ArrayList;
 
 public class ContagemRepetida {
-    
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		
+		int entrada =sc.nextInt();
+		while(entrada --> 0)
+			lista.add(sc.nextInt());
+		
+		SortedMap<Integer, Integer> contaQuant = new TreeMap<>();
+		lista.forEach(id -> contaQuant.compute(id, (key , val) -> (val == null ? 1 : val  + 1)));
+		
+		contaQuant.entrySet().forEach(entry->{
+			System.out.printf("%d aparece %d vez(es)\n", entry.getKey(), entry.getValue());  
+		});
+		
+		sc.close();
+	}
 }
